6ed07205f9b139da67ca19cd55d42702fa45491f
Douglas Miller <durgadas311@gmail.com>
2019-11-17 09:56:26 -0600

    Align breaks better with original, add framework for radix and style.

--- dz/Analyzer.java
+++ dz/Analyzer.java
@@ -17,6 +17,8 @@ public class Analyzer implements Memory {
 	String basePath;
 	byte[] obj;
 	byte[] brk;	// breaks
+	byte[] rdx;	// radix
+	byte[] sty;	// style
 	byte[] len;	// length of "instructions" (lines)
 	Map<Integer,String> symtab;
 	Vector<Integer> codes;
@@ -114,9 +116,15 @@ public class Analyzer implements Memory {
 			case 'C':
 				len += 1;
 				break;
-			case 'L':
-			case 'T':
+			case 'L':	// label
+			case '"':	// double-byte
 				len += 2;
+				break;
+			case 'T':	// "table", byte key
+				len += 3;	// char + label
+				break;
+			case 'Q':	// "table", word key
+				len += 4;	// word + label
 				break;
 			case '7':
 				len += 1;	// at least 1?
@@ -126,32 +134,68 @@ public class Analyzer implements Memory {
 		return len;
 	}
 
+	private int putLabel(int adr) {
+		putBrk(adr, 'L');
+		int lbl = read(adr) | (read(adr + 1) << 8);
+		if (lbl >= base && lbl < end) {
+			orphans.add(lbl);
+			lookup(true, lbl);
+		}
+		adr += 2;
+		return adr;
+	}
+
 	private int putStruct(int adr, int chr) {
 		int lbl;
 		switch (chr) {
+		case ' ':
+			break;
+		case '"':
+			putBrk(adr, 'B');
+			adr += 2;
+			break;
 		case 'C':
-			putBrk(adr, 'C');
+		case 'B':
+			putBrk(adr, 'B');
 			adr += 1;
 			break;
-		case 'T':
-			putBrk(adr, 'T');
+		case 'Q':
+			putBrk(adr, 'W');
 			adr += 2;
+			adr = putLabel(adr);
+			break;
+		case 'T':
+			putBrk(adr, 'B');
+			adr += 1;
+			adr = putLabel(adr);
 			break;
 		case 'L':
-			putBrk(adr, 'L');
-			lbl = read(adr) | (read(adr + 1) << 8);
-			if (lbl >= base && lbl < end) {
-				orphans.add(lbl);
-				lookup(true, lbl);
-			}
-			adr += 2;
+			adr = putLabel(adr);
 			break;
 		case '7':
 			putBrk(adr, '7');
 			while ((read(adr++) & 0x80) == 0);
 			break;
+		default:
+			// TODO: more required?
+			putBrk(adr, chr);
+			break;
 		}
 		return adr;
+	}
+
+	private int putStruct(int adr, String brk) {
+		if (brk.length() > 1) {
+			putStyle(adr, brk.charAt(1));
+		}
+		if (brk.length() > 2) {
+			putRadix(adr, brk.charAt(2));
+		}
+		if (brk.length() > 0) {
+			return putStruct(adr, brk.charAt(0));
+		} else {
+			return adr;	// not possible?
+		}
 	}
 
 	private void markStruct(int start, int end, String str) {
@@ -195,6 +239,13 @@ public class Analyzer implements Memory {
 			}
 			orphans.add(b);
 			break;
+		case '-':	// simple break
+			if (ss.length < 2 || ss[1].length() < 1) {
+				return false;
+			}
+			// TODO: complex break settings (B[MCN], B[HD2])
+			putStruct(a, ss[1]);
+			break;
 		case '+':
 			codes.add(a);
 			break;
@@ -212,6 +263,9 @@ public class Analyzer implements Memory {
 			}
 			calls.put(a, c);
 			break;
+		case ';':	// TODO: major comment
+		case '/':	// TODO: minor comment
+			break;
 		default:
 			return false;
 		}
@@ -223,6 +277,8 @@ public class Analyzer implements Memory {
 			InputStream f = new FileInputStream(com);
 			obj = new byte[f.available()];
 			brk = new byte[f.available()];
+			rdx = new byte[f.available()];
+			sty = new byte[f.available()];
 			len = new byte[f.available()];
 			f.read(obj);
 			f.close();
@@ -325,8 +381,24 @@ public class Analyzer implements Memory {
 		return brk[a - base] & 0xff;
 	}
 
+	private int getRadix(int a) {
+		return rdx[a - base] & 0xff;
+	}
+
+	private int getStyle(int a) {
+		return sty[a - base] & 0xff;
+	}
+
 	private void putBrk(int a, int b) {
 		brk[a - base] = (byte)b;
+	}
+
+	private void putRadix(int a, int b) {
+		rdx[a - base] = (byte)b;
+	}
+
+	private void putStyle(int a, int b) {
+		sty[a - base] = (byte)b;
 	}
 
 	private String lookup(int a) {
@@ -377,17 +449,31 @@ public class Analyzer implements Memory {
 	private void generateDZ(File dz, int first, int last) throws Exception {
 		PrintStream ps = new PrintStream(dz);
 		int bk = 0;
+		int st = 0;
+		int rx = 0;
 		for (int a = first; a < last; ++a) {
 			String l = lookup(a);
 			if (l != null) {
 				ps.format("%s\n", l);
 			}
+			String s = "";
 			int b = getBrk(a);
-			if (b != 0) {
-				if (bk != b) {
-					ps.format("-%04x,%c\n", a, b);
-				}
+			if (b != 0 && bk != b) {
+				s += (char)b;
 				bk = b;
+			} else s += " ";
+			b = getStyle(a);
+			if (b != 0 && st != b) {
+				s += (char)b;
+				st = b;
+			} else s += " ";
+			b = getRadix(a);
+			if (b != 0 && rx != b) {
+				s += (char)b;
+				rx = b;
+			} else s += " ";
+			if (!s.equals("   ")) {
+				ps.format("-%04x,%s\n", a, s);
 			}
 		}
 		ps.close();
@@ -403,6 +489,7 @@ public class Analyzer implements Memory {
 		Arrays.fill(len, (byte)0);
 		clearSymtab();
 		while ((s = lin.readLine()) != null) {
+			// TODO: fix this
 			if (s.matches("-[0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F],.")) {
 				a = Integer.valueOf(s.substring(1,5), 16);
 				bk = s.charAt(6);
@@ -423,6 +510,18 @@ public class Analyzer implements Memory {
 		return a;
 	}
 
+	// returns address of first radix before or at address 'a'
+	private int activeRadix(int a) {
+		while (a > base && getRadix(a) == 0) --a;
+		return a;
+	}
+
+	// returns address of first style before or at address 'a'
+	private int activeStyle(int a) {
+		while (a > base && getStyle(a) == 0) --a;
+		return a;
+	}
+
 	private String disLineInstr(Z80Dissed d) {
 		if (d.addr < 0) {
 			if (d.fmt == null) {
@@ -440,52 +539,102 @@ public class Analyzer implements Memory {
 		}
 	}
 
+	private String printBinary(int v, int w) {
+		// This is ugly...
+		String s = Integer.toBinaryString(v);
+		while (s.length() < w) {
+			s = '0' + s;
+		}
+		return s;
+	}
+
+	private String printWord(int w, int rdx) {
+		if (w == 0) {
+			return "0";
+		}
+		if (rdx == 'D') {
+			// TODO: signed?
+			return String.format("%d", w);
+		}
+		if (rdx == '2') {
+			return printBinary(w, 16);
+		}
+		// default = hex
+		return String.format("0%04xh", w);
+	}
+
+	private String printByte(int b, int rdx) {
+		if (b == 0) {
+			return "0";
+		}
+		if (rdx == 'D') {
+			// TODO: signed?
+			return String.format("%d", b);
+		}
+		if (rdx == '2') {
+			return printBinary(b, 8);
+		}
+		// default = hex
+		return String.format("0%02xh", b);
+	}
+
+	private String disLineWord(int a, int rdx) {
+		int l = read(a) | (read(a + 1) << 8);
+		String w = printWord(l, rdx);
+		return "dw\t" + w;
+	}
+
 	private String disLineLabel(int a) {
 		int l = read(a) | (read(a + 1) << 8);
 		String s = lookup(l);
 		if (s != null) {
 			return "dw\t" + s;
 		} else {
+			// TODO: honor rdx?
 			return String.format("dw\t0%04xh", l);
 		}
 	}
 
-	private String disLineHex(int a, int n) {
+	private String disLineNum(int a, int n, int rdx) {
 		String s = "";
 		int c;
 		while (n > 0) {
 			c = read(a);
 			if (s.length() > 0) s += ',';
-			s += String.format("0%02xh", c);
+			s += printByte(c, rdx);
 			++a;
 			--n;
 		}
 		return "db\t" + s;
 	}
 
-	private String disLineData(int a, int n, boolean bit7) {
+	private String disLineData(int a, int n, int rdx) {
 		String s = "";
 		boolean q = false;
+		boolean bit7 = (rdx == '7');
+		// TODO: honor rdx H,D,2... and style M,N
 		int c;
+		int e;
 		while (n > 0) {
 			c = read(a);
-			if (c < ' ') {
+			if (bit7 && n == 1) {
+				e = (c & 0x7f);
+			} else {
+				e = c;
+			}
+			if (e < ' ' || e > '~') {
 				if (q) { s += '\''; q = false; }
 				if (s.length() > 0) s += ',';
-				s += String.format("%d", c);
-			} else if (!bit7 && c > '~') {
-				if (q) { s += '\''; q = false; }
-				if (s.length() > 0) s += ',';
-				s += String.format("0%02xh", c);
+				s += printByte(e, rdx);
 			} else {
 				if (!q) {
 					if (s.length() > 0) s += ',';
 					s += '\'';
 					q = true;
 				}
-				s += (char)(c & 0x7f);
-				if ((c & 0x7f) == '\'') s += (char)(c & 0x7f);
-				if ((c & 0x80) != 0) {
+				s += (char)e;
+				if (e == '\'') s += (char)e;
+				if (bit7 && n == 1 && (c & 0x80) != 0) {
 					s += "'+80h";
 					q = false;
 				}
@@ -507,10 +656,16 @@ public class Analyzer implements Memory {
 		int ba = activeBreak(first);
 		int bk = getBrk(ba);
 		if (bk == 0) bk = 'I';
+		// TODO: need to override sometimes...
+		ba = activeRadix(first);
+		int rx = getRadix(ba);
+		if (rx == 0) rx = 'H';
 		for (int a = first; a < last;) {
 			b = getBrk(a);
 			if (b != 0) bk = b;
 			if (b == 0) b = ' ';
+			n = getRadix(a);
+			if (n != 0) rx = n;
 			l = lookup(a);
 			if (l != null) ps.format("%s:", l);
 			//	if (l.length() > 7) ps.format("\n");
@@ -526,23 +681,23 @@ public class Analyzer implements Memory {
 			} else if (bk == 'L') {
 				ps.format("%s", disLineLabel(a));
 				a += 2;
+			} else if (bk == 'W') {
+				ps.format("%s", disLineWord(a, rx));
+				a += 2;
 			} else if (bk == 'C') {
-				ps.format("%s", disLineData(a, 1, false));
+				ps.format("%s", disLineData(a, 1, rx));
 				a += 1;
 			} else if (bk == '7') {
 				for (z = 1; a + z < last; ++z) {
 					if (getBrk(a + z) != 0) break;
 				}
-				ps.format("%s", disLineData(a, z, true));
+				ps.format("%s", disLineData(a, z, '7'));
 				a += z;
-			} else if (bk == 'T') {
-				ps.format("%s", disLineHex(a, 2));
-				a += 2;
 			} else {
 				for (z = 1; z < 16 && a + z < end; ++z) {
 					if (getBrk(a + z) != 0) break;
 				}
-				ps.format("%s", disLineData(a, z, false));
+				ps.format("%s", disLineData(a, z, rx));
 				a += z;
 			}
 			ps.format("\n");
