56bf92102b4e2c81bdee6c904bbfca6b3c07b707
Douglas Miller <durgadas311@gmail.com>
2019-11-28 18:06:21 -0600

    Update dzdecode to handle dzinstal.com

--- disas/dzdecode.c
+++ disas/dzdecode.c
@@ -13,6 +13,17 @@ uint8_t obj[64*1024];
 int tbl[128][3] = {0};
 int max;
 
+struct com {
+	int csbeg;
+	int csend;
+	int tbl;
+} coms[] = {
+// For DZ.COM...
+[0] = { 0x0300, 0x8fb7, 0x8f0b },
+// For DZINSTAL.COM...
+[1] = { 0x0103, 0x8a7f, 0x8973 },
+};
+
 static void lddr(int hl, int de, int bc) {
 	do {
 		if (de > max) max = de;
@@ -38,14 +49,14 @@ static int word(int a) {
 }
 
 // boolean
-static int checksum() {
+static int checksum(struct com *com) {
 	int de;
 	int hl = 0;
-	for (de = 0x0300; de < 0x8fb7; ++de) {
+	for (de = com->csbeg; de < com->csend; ++de) {
 		hl += obj[de];
 	}
 	hl &= 0xffff;
-	return (hl == word(0x8fb7));
+	return (hl == word(com->csend));
 }
 
 int main(int argc, char **argv) {
@@ -55,35 +66,48 @@ int main(int argc, char **argv) {
 	int de;
 	int bc;
 	int x;
+	int install = 0;
+
+	extern int optind;
+	extern char *optarg;
 
 	memset(obj, 0, sizeof(obj));
 
-	if (argc < 3) {
-		fprintf(stderr, "usage: %s <dz.com> <outfile>\n", argv[0]);
+	while ((x = getopt(argc, argv, "i")) != EOF) {
+		switch(x) {
+		case 'i':
+			install = 1;
+			break;
+		}
+	}
+	x = optind;
+
+	if (argc - optind < 2) {
+		fprintf(stderr, "usage: %s [-t <addr>] <dz.com> <outfile>\n", argv[0]);
 		exit(1);
 	}
-	fd = open(argv[1], O_RDONLY);
+	fd = open(argv[optind], O_RDONLY);
 	if (fd < 0) {
-		perror(argv[1]);
+		perror(argv[optind]);
 		exit(1);
 	}
 	n = read(fd, obj + 0x0100, sizeof(obj) - 0x0100);
 	if (n < 0) {
-		perror(argv[1]);
+		perror(argv[optind]);
 		exit(1);
 	}
-	if (n < 0x8f00) {
-		fprintf(stderr, "DZ.COM too small\n");
+	if (n + 0x0100 < coms[install].tbl) {
+		fprintf(stderr, "COM too small\n");
 		exit(1);
 	}
 	close(fd);
-	if (!checksum()) {
-		fprintf(stderr, "DZ.COM corrupted\n");
+	if (!checksum(&coms[install])) {
+		fprintf(stderr, "COM corrupted\n");
 		exit(1);
 	}
 	max = 0;
 	// copy table to safety...
-	n = 0x8f0b;
+	n = coms[install].tbl;
 	x = 0;
 	do {
 		hl = word(n);
@@ -110,18 +134,18 @@ int main(int argc, char **argv) {
 	put_byte(0x0100, 0xc3);	// JMP
 	put_word(0x0101, tbl[x][0]); // new start/entry point
 	max += 1;	// bound
-	fd = open(argv[2], O_WRONLY|O_CREAT|O_TRUNC, 0666);
+	fd = open(argv[optind + 1], O_WRONLY|O_CREAT|O_TRUNC, 0666);
 	if (fd < 0) {
-		perror(argv[2]);
+		perror(argv[optind + 1]);
 		exit(1);
 	}
 	n = write(fd, obj + 0x0100, max - 0x0100);
 	if (n < 0) {
-		perror(argv[2]);
+		perror(argv[optind + 1]);
 		exit(1);
 	}
 	if (close(fd) < 0) {
-		perror(argv[2]);
+		perror(argv[optind + 1]);
 		exit(1);
 	}
 	printf("done. entry = 0x%04x\n", tbl[x][0]);
