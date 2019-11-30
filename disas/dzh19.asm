b3770c8ec1ac0a0d2ae6bfb3866da1fea5024e73
Douglas Miller <durgadas311@gmail.com>
2019-11-29 20:40:26 -0600

    Enhance/define primary config/term-def examples

--- disas/dzh19.asm
+++ disas/dzh19.asm
@@ -25,57 +25,60 @@
 esc	equ	27
 
 	org	0100h
-	ds	3	; entry point - not disturbed
+	ds	3	; entry point - do not disturb
 	ds	2
 	ds	2
 	ds	1	; 0107
 	ds	1
 	ds	1
 	ds	56	; copyright message (len,str)
-	db	1ah	; terminator? marker?
-; -------------------
-; 0143:
-	db	7,'H19/H89',0	; length, term name, NUL terminator
+
+ if ($ <> 0142h)
+	.error "ConfigBegin Fail"
+ endif
+; ConfigBegin
+	db	1ah	; 0142: terminator? marker?
+	db	7,'H19/H89',0	; 0143: len, term name, NUL term
 	rept	017ch-$	;  55-chars max for terminal name (+len,NUL)
 	db	0
 	endm
 
 	db	0ffh	; 017c: menus rev vid (0x80)
-	db	80	; 017d:	'P'
-	db	17	; 017e: disp cnt for 2nd menus
+	db	80	; 017d:	delay for blink (*8)
+	db	17	; 017e: delay for 2ndry menus (*128)
 	db	3	; 017f: help level
 
-	db	' '	; 0180:
-	db	0	; 0181:
-	db	1	; 0182:
-	db	127	; 0183: 80-ff undisplayable (0x80)
-	db	127	; 0184: bit7 in ascii win (0x80)
-	db	127	; 0185:
-	db	95	; 0186:
+	db	20h	; 0180: CP/M version (>= 2.0)
+	db	0	; 0181: OF on/off
+	db	1	; 0182: OU on/ff
+	db	7fh	; 0183: 80-ff undisplayable (0x80)
+	db	7fh	; 0184: bit7 in ascii win (0x80)
+	db	7fh	; 0185: disp char limit (7eh=Hazeltine/7fh)
+	db	'_'	; 0186: char for undisplayables
 	db	1	; 0187: hilite in hex win
 	db	1	; 0188: hilite in ascii win
-	db	18	; 0189: instr last
-	db	20	; 018a: hex first
-	db	23	; 018b: hex last
-	db	1	; 018c:	rel jmp adds "; $+"
+	db	18	; 0189: instr win last line
+	db	20	; 018a: hex win first line
+	db	23	; 018b: hex win last line
+	db	1	; 018c:	rel jmp adds "; $+xx"
 
-	db	1fh	; 018d: alt down-arrow
-	db	1eh	; 018e: alt up-arrow
-	db	22,26,31,39,45	; 018f: KP tabs
-	db	0,8,16,24,32	; 0194: KW tabs
+	db	1fh	; 018d: alt down-arrow key
+	db	1eh	; 018e: alt up-arrow key
+	db	22,26,31,39,45	; 018f: LST tab stops
+	db	0,8,16,24,32	; 0194: ASM tab stops
 
-	db	4,'LST:'	; 0199: printer
+	db	4,'LST:'	; 0199: printer device
 	db	4,'.MAC'	; 019e: ASM suffix
 	db	1		; 01a3: SA interative pref
-	db	72		; 01a4: screen right
-	db	70		; 01a5:	list right
-	db	16		; 01a6:	word break
+	db	72		; 01a4: screen right col
+	db	70		; 01a5:	list right col
+	db	16		; 01a6:	word break (threshold)?
 	db	7		; 01a7: cache
-	db	1,'D',0,0	; 01a8:
-	db	'C'		; 01ac:
-	db	0		; 01ad:	LST: shift
-	db	'H'		; 01ae: radix (for?)
-	db	'B'		; 01af: radix (for?)
+	db	1,'D',0,0	; 01a8:	(BC mode? N/U?)
+	db	'C'		; 01ac:	(BC mode? N/U?)
+	db	0		; 01ad:	LST: left margin
+	db	'H'		; 01ae: suffix for hex const
+	db	'B'		; 01af: suffix for bin const
 
 	; multi-language support:
 	db	12,'DZENGMNU.OVR',0,0	; 01b0: Menus file
@@ -103,20 +106,20 @@ esc	equ	27
 	db	2,esc,'p',0,0,0,0,0,0,0,0,0,0,0,0,0	; 0275: rev video
 	db	2,esc,'q',0,0,0,0,0,0,0,0,0,0,0,0,0	; 0285: norm video
 
-	db	08	; 0295:
-	db	00	; 0296:
-	db	00	; 0297:
-	dw	00	; 0298:
-	db	00	; 029a:
-	db	00	; 029b:
-	db	00	; 029c:
+	db	8	; 0295: n/c
+	db	1	; 0296: last screen char usable
+	db	0	; 0297:	patch, non-zero flag for video RAM
+	dw	0	; 0298: patch, video RAM addr
+	db	0	; 029a: (?)
+	db	0	; 029b: n/c
+	db	0	; 029c: n/c
 
 	; hooks to customize functions
-	nop	; 029d:
+	nop	; 029d: video RAM map in hook
 	ret
 	nop
 
-	nop	; 02a0
+	nop	; 02a0: video RAM map out hook
 	ret
 	nop
 
@@ -136,20 +139,14 @@ esc	equ	27
 	ret
 	nop
 
-	db	0	; 02af:
-	db	0	; 02b0:
+	db	0	; 02af: n/c
+	db	0	; 02b0: n/c
 	db	1	; 02b1: keyboard scanning (1=on)
-	db	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0	; 02b2:
-	db	0,0,0,0,0,0,0,0,0,0,0,0,0,0	; 02c2:
-	db	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	db	107,0,0			; 02e0:
-	db	0,0,0,0,0,0,0,0,0	; 02e4:
-	db	0			; 02ed:
-	db	0			; 02ee:
-	db	0			; 02ef:
-	db	0			; 02f0:
-	db	0,0			; 02f1:
-	db	0,0,0,0,0,0,0		; 02f3:
-	db	0,0,0,0,0,0,0		; 02fa:	(0107)
-
+	db	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0	; 02b2: printer init
+	db	0,0,0,0,0,0,0,0,0,0,0,0,0,0	; 02c2: patch area...
+	db	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0	; ...02df
+; ConfigEnd
+ if ($ <> 02e0h)
+	.error "Overflow Config Area"
+ endif
 	end
--- disas/dzxterm.asm
+++ disas/dzxterm.asm
@@ -25,89 +25,128 @@
 esc	equ	27
 
 	org	0100h
-	ds	64
-	db	0,0,1ah
-;
-	db	5,'xterm',0	; length, term name, NUL terminator
+	ds	3	; entry point - do not disturb
+	ds	2
+	ds	2
+	ds	1	; 0107
+	ds	1
+	ds	1
+	ds	56	; copyright message (len,str)
+
+ if ($ <> 0142h)
+	.error "ConfigBegin Fail"
+ endif
+; ConfigBegin
+	db	1ah	; 0142: terminator? marker?
+	db	5,'xterm',0	; 0143: len, term name, NUL term
 	rept	017ch-$	;  55-chars max for terminal name (+len,NUL)
 	db	0
 	endm
 
-	db	0ffh,80,11h
-	db	3	; help level
+	db	0ffh	; 017c: menus rev vid (0x80)
+	db	80	; 017d:	delay for blink (*8)
+	db	17	; 017e: delay for 2ndry menus (*128)
+	db	3	; 017f: help level
 
-	db	' ',0,1,127,127,127,95,1,1,18,20,23,1
+	db	20h	; 0180: CP/M version (>= 2.0)
+	db	0	; 0181: OF on/off
+	db	1	; 0182: OU on/ff
+	db	7fh	; 0183: 80-ff undisplayable (0x80)
+	db	7fh	; 0184: bit7 in ascii win (0x80)
+	db	7fh	; 0185: disp char limit (7eh=Hazeltine/7fh)
+	db	'_'	; 0186: char for undisplayables
+	db	1	; 0187: hilite in hex win
+	db	1	; 0188: hilite in ascii win
+	db	18	; 0189: instr win last line
+	db	20	; 018a: hex win first line
+	db	23	; 018b: hex win last line
+	db	1	; 018c:	rel jmp adds "; $+xx"
 
-	db	1fh	; alt down-arrow
-	db	1eh	; alt up-arrow
-	db	22,26,31,39,45	; KP tabs
-	db	0,8,16,24,32	; KW tabs
+	db	1fh	; 018d: alt down-arrow key
+	db	1eh	; 018e: alt up-arrow key
+	db	22,26,31,39,45	; 018f: LST tab stops
+	db	0,8,16,24,32	; 0194: ASM tab stops
 
-	db	4,'LST:'	; printer
-	db	4,'.MAC'	; ASM suffix
-	db	1,'HF'
-	db	16,7
-	db	1,'D',0,0,'C',0,'HB'
+	db	4,'LST:'	; 0199: printer device
+	db	4,'.MAC'	; 019e: ASM suffix
+	db	1		; 01a3: SA interative pref
+	db	72		; 01a4: screen right col
+	db	70		; 01a5:	list right col
+	db	16		; 01a6:	word break (threshold)?
+	db	7		; 01a7: cache
+	db	1,'D',0,0	; 01a8:	(BC mode? N/U?)
+	db	'C'		; 01ac:	(BC mode? N/U?)
+	db	0		; 01ad:	LST: left margin
+	db	'H'		; 01ae: suffix for hex const
+	db	'B'		; 01af: suffix for bin const
 
 	; multi-language support:
-	db	12,'DZENGMNU.OVR',0,0	; Menus
-	db	12,'DZENGHLP.OVR',0,0	; Help
-	db	12,'DZENGMSG.OVR',0,0	; Messages
-	db	0e8h,3
+	db	12,'DZENGMNU.OVR',0,0	; 01b0: Menus file
+	db	12,'DZENGHLP.OVR',0,0	; 01bf: Help file
+	db	12,'DZENGMSG.OVR',0,0	; 01ce: Messages file
+	dw	1000		; 01dd: headroom
 
 	; terminal definition start?
-	db	24	; rows (lines)
-	db	80	; columns
-	db	13,esc,'[1;1H',esc,'[1;24r',0,0		; init seq
-	db	13,esc,'[r',esc,'[1;1H',esc,'[2J',0,0	; exit seq
-	db	2,esc,'[',0,0,0,0,0,0,0,0,0,0,0,0,0	; curs pre
-	db	1,';',0,0,0,0,0,0,0,0,0,0,0,0,0,0	; curs sep
-	db	1,'H',0,0,0,0,0,0,0,0,0,0,0,0,0,0	; curs end
-	db	0	; 0=row-first, 1=col-first
-	db	1	; row offset
-	db	1	; col offset
-	db	2	; r/c type, 0=binary, 2=ascii 2-dig, 3=ascii 3-dig
+	db	24	; 01df: rows (lines)
+	db	80	; 01e0: columns
+	db	14,esc,'[H',esc,'[2J',esc,'[1;24r',0		; 01e1: init seq
+	db	10,esc,'[r',esc,'[H',esc,'[2J',0,0,0,0,0	; 01f1: exit seq
+	db	2,esc,'[',0,0,0,0,0,0,0,0,0,0,0,0,0	; 0201: curs pre
+	db	1,';',0,0,0,0,0,0,0,0,0,0,0,0,0,0	; 0211: curs sep
+	db	1,'H',0,0,0,0,0,0,0,0,0,0,0,0,0,0	; 0221: curs end
+	db	0	; 0231: 0=row-first, 1=col-first
+	db	1	; 0232: row offset
+	db	1	; 0233: col offset
+	db	2	; 0234: r/c type, 0=binary, 2=ascii 2-dig, 3=ascii 3-dig
 
-	db	3,esc,'[K',0,0,0,0,0,0,0,0,0,0,0,0	; erase to EOL
-	db	3,esc,'[M',0,0,0,0,0,0,0,0,0,0,0,0	; delete line
-	db	3,esc,'[L',0,0,0,0,0,0,0,0,0,0,0,0	; insert line
-	db	4,esc,'[2J',0,0,0,0,0,0,0,0,0,0,0	; clear screen
-	db	4,esc,'[7m',0,0,0,0,0,0,0,0,0,0,0	; rev video
-	db	4,esc,'[0m',0,0,0,0,0,0,0,0,0,0,0	; norm video
+	db	3,esc,'[K',0,0,0,0,0,0,0,0,0,0,0,0	; 0235: erase to EOL
+	db	3,esc,'[M',0,0,0,0,0,0,0,0,0,0,0,0	; 0245: delete line
+	db	3,esc,'[L',0,0,0,0,0,0,0,0,0,0,0,0	; 0255: insert line
+	db	4,esc,'[2J',0,0,0,0,0,0,0,0,0,0,0	; 0265: clear screen
+	db	4,esc,'[7m',0,0,0,0,0,0,0,0,0,0,0	; 0275: rev video
+	db	4,esc,'[0m',0,0,0,0,0,0,0,0,0,0,0	; 0285: norm video
 
-	db	08,01,00,00,00,00,00,00
+	db	8	; 0295: n/c
+	db	1	; 0296: last screen char usable
+	db	0	; 0297:	patch, non-zero flag for video RAM
+	dw	0	; 0298: patch, video RAM addr
+	db	0	; 029a: (?)
+	db	0	; 029b: n/c
+	db	0	; 029c: n/c
 
 	; hooks to customize functions
-	nop
+	nop	; 029d: video RAM map in hook
 	ret
 	nop
 
-	nop
+	nop	; 02a0: video RAM map out hook
 	ret
 	nop
 
-	nop
+	nop	; 02a3
 	ret
 	nop
 
-	nop
+	nop	; 02a6
 	ret
 	nop
 
-	nop
+	nop	; 02a9
 	ret
 	nop
 
-	nop	; BIOS conout hook
+	nop	; 02ac: BIOS conout hook
 	ret
 	nop
 
-	db	0,0
-	db	1	; keyboard scanning (1=on)
-	db	00,00,00,00,00,00,00,00,00,00,00,00,00,00
-	db	00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00
-	db	00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00
-	db	'k',00,00,00,00,00,00,00,00,00,00,00,00,00,00,00
-	db	00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00
-
+	db	0	; 02af: n/c
+	db	0	; 02b0: n/c
+	db	1	; 02b1: keyboard scanning (1=on)
+	db	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0	; 02b2: printer init
+	db	0,0,0,0,0,0,0,0,0,0,0,0,0,0	; 02c2: patch area...
+	db	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0	; ...02df
+; ConfigEnd
+ if ($ <> 02e0h)
+	.error "Overflow Config Area"
+ endif
 	end
